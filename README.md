ff4adec60e71322824d85b96629df7af8d79c67a
ipiknipsi <danielrtml@gmail.com>
2021-11-05 09:14:00 -0300

    Se sube la versión final del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Entrega2_002D_RetamalOrme-o
 Este repositorio contiene la app con crud , api rest, y persistencia de datos, de la entrega 2 de la sección 002D del ramo Programacion de Apps. móviles.
+
+Pasos que realizamos en la aplicación.
+Primero en el cmd, nos dirigimos a la ruta de la App creada, en éste caso usamos ionic storage y firebase.
+Para instalar ionic Storage (la versión 3.0.6), en la misma ruta de nuestra app, ejecutamos el comando "npm install @ionic/storage" una vez finalizado éste paso,
+instalamos el plugin para trabajar ionic storage en Angular, el cual es "npm install @ionic/storage-angular. Luego de estos dos pasos, generamos un servicio en la misma consola
+el comando es ionic generate service services/"y el nombre del archivo" en nuestro caso lo nombramos servicedatos, al final se importan el Ionic Storage Module y los Drivers desde @ionic/storage.
+todo esto en nuestro app.module.ts
+
+eso hicimos en Ionic Storage, ahora con Firebase, los pasos son muy similares, en el cmd en la ruta de la app
+Primero nos registramos en el sitio web de Firebase, creamos nuestro proyecto e importamos la api y la apiKey que nos genera, luego instalamos
+ el plugin de firebase para angular, con el comando npm install firebase @angular/fire , posterior a eso generamos el service, para la autentificación de nuestro login.
+Ademas de generar un Guard, que nos servirá para exigir el Login en ciertos Pages. Finalmente importamos los modulos de AngularFireModule y Angular Fire Auth Module, desde @angular/fire.
+Una vez ya listo, continuamos nuestro desarrollo de la app.
+Integrantes del Grupo:
+			-Daniel Retamal
+			-Matias Ormeño
+
+Nombre del Proyecto: Yapue.
---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+
+
+}
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+  
+})
+export class InicioPage implements OnInit {
+  @ViewChild("header") header: HTMLElement;
+
+  componentes : Componente[] = [
+    {
+      icon: 'home-outline',
+      name: 'Registrarse',
+      redirecTo: '/formulario',
+      
+    },
+    {
+      icon: 'storefront-outline',
+      name: 'Publicar',
+      redirecTo: '/publicar',
+      
+
+    },
+    {
+      icon: 'terminal-outline',
+      name: 'Formulario',
+      redirecTo: '/publicar',
+     
+    },
+    {
+      icon: 'chatbox-outline',
+      name: 'Mensajes',
+      redirecTo: '/inicio'
+    },
+    {
+      icon: 'bhelp-circle-outline',
+      name: 'Ayuda',
+      redirecTo: '/inicio'
+    },
+    
+    {
+      icon: 'apps-outline',
+      name: 'Api',
+      redirecTo: '/api'
+    },
+    {
+      icon: 'apps-outline',
+      name: 'Datos',
+      redirecTo: '/datos'
+    },
+    {
+      icon: 'apps-outline',
+      name: 'Login',
+      redirecTo: '/login'
+    },
+    {
+      icon: 'person-add-outline',
+      name: 'Registro',
+      redirecTo: '/registro'
+    },
+
+  ];
+  
+
+
+
+
+  constructor(private menuController: MenuController, public element: ElementRef,public renderer: Renderer2) { 
+    
+    
+
+
+  }
+
+  ngOnInit() {
+  }
+
+
+  mostrarMenu()
+  {
+    this.menuController.open('first')
+  }
+
+
+  
+//estudiar scroll events.
+
+  /*
+  
+  ionViewWillEnter() {
+    this.renderer.setStyle(this.header['el'], 'webkitTransition', 'top 700ms');
+  }
+  
+  
+  
+  
+  
+  onContentScroll(event) {
+    if (event.detail.scrollTop >= 50) {
+      this.renderer.setStyle(this.header['el'], 'top', '-76px');
+    } else {
+      this.renderer.setStyle(this.header['el'], 'top', '20px');
+    }
+  }*/
+
+
+
+
+}
